1762026785aeda000c26bc175990ec403caaa737
Rusydina Novitasari <rnvitas697@gmail.com>
2024-11-13 03:23:17 +0700

    config db

--- db/index.js
+++ db/index.js
@@ -1,6 +1,22 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const dbUrl = process.env.DB_URL;
-const db = mongoose.connect(dbUrl);
+
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 45000,
+  serverSelectionTimeoutMS: 5000,
+};
+
+const db = mongoose
+  .connect(dbUrl, dbOptions)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
 
 module.exports = db;
